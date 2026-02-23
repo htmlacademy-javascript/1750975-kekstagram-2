862ae41d1dd9cdc5f37c65a2558876c90b3bf4c4
Skripkina-Lyudmila <msk.revizor@gmail.com>
2026-02-23 14:07:50 +0300

    Добавляет комментарии JSDoc

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,35 @@
 'Use strict';
 
-// Функция для проверки длины строки: true, если строка меньше или равна указанной длине, false, если строка длиннее
-const isStringLengthValid = (string = '', maxSymbols = 1) => string.length <= maxSymbols;
+/**
+ * Функция для проверки длины строки
+ * @param {string} string - строка, которую нужно проверить
+ * @param {number} maxSymbols - максимально допустимая длина строки
+ * @returns {boolean} возвращает true, если строка меньше или равна параметру максимальной длины
+*/
 
-// Функция для проверки, является ли строка палиндромом
-const isPalindrome = (string = '') => {
+export const isStringLengthValid = (string = '', maxSymbols = -1) => string.length <= maxSymbols;
+
+/**
+ * Функция проверки, является ли строка палиндромом
+ * @param {string} string - строка, которую нужно проверить
+ * @returns {boolean}
+*/
+
+export const isPalindrome = (string = '') => {
   const normalizeString = string.replace(/\s+/g, '').toLowerCase();
   if (normalizeString.length === 0) {
     return false;
   }
   return normalizeString === normalizeString.split('').reverse().join('');
-}
+};
 
-// Функция извлекает из строки цифры и возвращает их в виде целого положительного числа
-const extractNumber = (input = '') => {
+/**
+ * Функция принимает строку и извлекает содержащиеся в ней цифры от 0 до 9
+ * @param {string} input - значение, которое нужно проверить
+ * @returns {number} возвращает извлеченные цифры в виде целого положительного числа
+*/
+
+export const extractNumber = (input = '') => {
   const string = String(input);
-  return Math.abs(parseInt(string.replace(/\D+/g, ''), 10));
-}
+  return parseInt(string.replace(/\D+/g, ''), 10);
+};
